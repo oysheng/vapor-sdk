31d8313ef8a79d12e04ed79c6b98f7bb65ef7eb7
oys <oys@oysdeMacBook-Pro.local>
2019-08-12 16:25:25 +0800

    add bytom decode raw tx

---
+++ bytom/transaction.go
@@ -0,0 +1,151 @@
+package transaction
+
+import (
+	"encoding/hex"
+	"encoding/json"
+
+	"github.com/bytom/blockchain/txbuilder"
+	"github.com/bytom/common"
+	"github.com/bytom/consensus"
+	"github.com/bytom/consensus/segwit"
+	"github.com/bytom/protocol/bc"
+	"github.com/bytom/protocol/bc/types"
+	"github.com/bytom/protocol/vm/vmutil"
+
+	"github.com/vapor-sdk/util"
+)
+
+// BytomDecodeRawTx decode raw transaction
+func BytomDecodeRawTx(rawTransaction string) []byte {
+	var rawTx types.Tx
+	if err := rawTx.UnmarshalText([]byte(rawTransaction)); err != nil {
+		return nil
+	}
+
+	tx := &util.Transaction{
+		TxID:      rawTx.ID.String(),
+		Version:   int64(rawTx.Version),
+		Size:      int64(rawTx.SerializedSize),
+		TimeRange: int64(rawTx.TimeRange),
+		Inputs:    []util.AnnotatedInput{},
+		Outputs:   []util.AnnotatedOutput{},
+		Fee:       int64(txbuilder.CalculateTxFee(&rawTx)),
+	}
+
+	for i := range rawTx.Inputs {
+		tx.Inputs = append(tx.Inputs, buildAnnotatedInput(&rawTx, uint32(i)))
+	}
+	for i := range rawTx.Outputs {
+		tx.Outputs = append(tx.Outputs, buildAnnotatedOutput(&rawTx, i))
+	}
+
+	jsonTx, err := json.Marshal(tx)
+	if err != nil {
+		return nil
+	}
+	return jsonTx
+}
+
+// buildAnnotatedInput build the annotated input.
+func buildAnnotatedInput(tx *types.Tx, i uint32) util.AnnotatedInput {
+	orig := tx.Inputs[i]
+	in := util.AnnotatedInput{}
+	if orig.InputType() != types.CoinbaseInputType {
+		assetID := orig.AssetID()
+		in.AssetID = assetID.String()
+		in.Amount = int64(orig.Amount())
+		signData := tx.SigHash(i)
+		in.SignData = signData.String()
+	} else {
+		in.AssetID = consensus.BTMAssetID.String()
+	}
+
+	id := tx.Tx.InputIDs[i]
+	in.InputID = id.String()
+	e := tx.Entries[id]
+	switch e := e.(type) {
+	case *bc.Spend:
+		in.Type = "spend"
+		controlProgram := orig.ControlProgram()
+		in.ControlProgram = hex.EncodeToString(controlProgram)
+		in.Address = getAddressFromControlProgram(controlProgram)
+		in.SpentOutputID = e.SpentOutputId.String()
+		arguments := orig.Arguments()
+		for _, arg := range arguments {
+			in.WitnessArguments = append(in.WitnessArguments, hex.EncodeToString(arg))
+		}
+
+	case *bc.Issuance:
+		in.Type = "issue"
+		issuanceProgram := orig.IssuanceProgram()
+		in.IssuanceProgram = hex.EncodeToString(issuanceProgram)
+		arguments := orig.Arguments()
+		for _, arg := range arguments {
+			in.WitnessArguments = append(in.WitnessArguments, hex.EncodeToString(arg))
+		}
+		if assetDefinition := orig.AssetDefinition(); isValidJSON(assetDefinition) {
+			in.AssetDefinition = hex.EncodeToString(assetDefinition)
+		}
+
+	case *bc.Coinbase:
+		in.Type = "coinbase"
+		in.Arbitrary = hex.EncodeToString(e.Arbitrary)
+	}
+	return in
+}
+
+// buildAnnotatedOutput build the annotated output.
+func buildAnnotatedOutput(tx *types.Tx, idx int) util.AnnotatedOutput {
+	orig := tx.Outputs[idx]
+	outid := tx.OutputID(idx)
+	out := util.AnnotatedOutput{
+		OutputID:       outid.String(),
+		Position:       idx,
+		AssetID:        orig.AssetId.String(),
+		Amount:         int64(orig.Amount),
+		ControlProgram: hex.EncodeToString(orig.ControlProgram),
+		Address:        getAddressFromControlProgram(orig.ControlProgram),
+	}
+
+	if vmutil.IsUnspendable(orig.ControlProgram) {
+		out.Type = "retire"
+	} else {
+		out.Type = "control"
+	}
+	return out
+}
+
+func isValidJSON(b []byte) bool {
+	var v interface{}
+	err := json.Unmarshal(b, &v)
+	return err == nil
+}
+
+func getAddressFromControlProgram(prog []byte) string {
+	if segwit.IsP2WPKHScript(prog) {
+		if pubHash, err := segwit.GetHashFromStandardProg(prog); err == nil {
+			return buildP2PKHAddress(pubHash)
+		}
+	} else if segwit.IsP2WSHScript(prog) {
+		if scriptHash, err := segwit.GetHashFromStandardProg(prog); err == nil {
+			return buildP2SHAddress(scriptHash)
+		}
+	}
+	return ""
+}
+
+func buildP2PKHAddress(pubHash []byte) string {
+	address, err := common.NewAddressWitnessPubKeyHash(pubHash, &consensus.ActiveNetParams)
+	if err != nil {
+		return ""
+	}
+	return address.EncodeAddress()
+}
+
+func buildP2SHAddress(scriptHash []byte) string {
+	address, err := common.NewAddressWitnessScriptHash(scriptHash, &consensus.ActiveNetParams)
+	if err != nil {
+		return ""
+	}
+	return address.EncodeAddress()
+}
---
+++ bytom/transaction_test.go
@@ -0,0 +1,82 @@
+package transaction
+
+import (
+	"encoding/json"
+	"errors"
+	"testing"
+
+	"github.com/bytom/testutil"
+
+	"github.com/vapor-sdk/util"
+)
+
+func TestBytomDecodeRawTx(t *testing.T) {
+	cases := []struct {
+		desc           string
+		rawTransaction string
+		wantTx         *util.Transaction
+	}{
+		{
+			rawTransaction: `070100010161015fc8215913a270d3d953ef431626b19a89adf38e2486bb235da732f0afed515299ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff8099c4d59901000116001456ac170c7965eeac1cc34928c9f464e3f88c17d8630240b1e99a3590d7db80126b273088937a87ba1e8d2f91021a2fd2c36579f7713926e8c7b46c047a43933b008ff16ecc2eb8ee888b4ca1fe3fdf082824e0b3899b02202fb851c6ed665fcd9ebc259da1461a1e284ac3b27f5e86c84164aa518648222602013effffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff80bbd0ec980101160014c3d320e1dc4fe787e9f13c1464e3ea5aae96a58f00013cffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff8084af5f01160014bb93cdb4eca74b068321eeb84ac5d33686281b6500`,
+			wantTx: &util.Transaction{
+				TxID:      "4c97d7412b04d49acc33762fc748cd0780d8b44086c229c1a6d0f2adfaaac2db",
+				Version:   1,
+				Size:      332,
+				TimeRange: 0,
+				Inputs: []util.AnnotatedInput{
+					util.AnnotatedInput{
+						Type:           "spend",
+						InputID:        "9963265eb601df48501cc240e1480780e9ed6e0c8f18fd7dd57954068c5dfd02",
+						AssetID:        "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
+						Amount:         41250000000,
+						ControlProgram: "001456ac170c7965eeac1cc34928c9f464e3f88c17d8",
+						Address:        "bm1q26kpwrrevhh2c8xrfy5vnaryu0ugc97c3j896t",
+						SpentOutputID:  "01bb3309666618a1507cb5be845b17dee5eb8028ee7e71b17d74b4dc97085bc8",
+						WitnessArguments: []string{
+							"b1e99a3590d7db80126b273088937a87ba1e8d2f91021a2fd2c36579f7713926e8c7b46c047a43933b008ff16ecc2eb8ee888b4ca1fe3fdf082824e0b3899b02",
+							"2fb851c6ed665fcd9ebc259da1461a1e284ac3b27f5e86c84164aa5186482226",
+						},
+						SignData: "8d2bb534c819464472a94b41cea788e97a2c9dae09a6cb3b7024a44ce5a27835",
+					},
+				},
+				Outputs: []util.AnnotatedOutput{
+					util.AnnotatedOutput{
+						Type:           "control",
+						OutputID:       "567b34857614d16292220beaca16ce34b939c75023a49cc43fa432fff51ca0dd",
+						Position:       0,
+						AssetID:        "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
+						Amount:         41030000000,
+						ControlProgram: "0014c3d320e1dc4fe787e9f13c1464e3ea5aae96a58f",
+						Address:        "bm1qc0fjpcwuflnc06038s2xfcl2t2hfdfv07hgf77",
+					},
+					util.AnnotatedOutput{
+						Type:           "control",
+						OutputID:       "a8069d412e48c2b2994d2816758078cff46b215421706b4bad41f72a32928d92",
+						Position:       1,
+						AssetID:        "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
+						Amount:         200000000,
+						ControlProgram: "0014bb93cdb4eca74b068321eeb84ac5d33686281b65",
+						Address:        "bm1qhwfumd8v5a9sdqepa6uy43wnx6rzsxm9cp6j43",
+					},
+				},
+				Fee: 20000000,
+			},
+		},
+	}
+
+	for i, c := range cases {
+		jsonTx := BytomDecodeRawTx(c.rawTransaction)
+		if jsonTx == nil {
+			t.Fatal(errors.New("error"))
+		}
+
+		gotTx := &util.Transaction{}
+		if err := json.Unmarshal(jsonTx, gotTx); err != nil {
+			t.Fatal(err)
+		}
+
+		if !testutil.DeepEqual(gotTx, c.wantTx) {
+			t.Errorf("case #%d, annotated transaction got=%#v, want=%#v", i, gotTx, c.wantTx)
+		}
+	}
+}
